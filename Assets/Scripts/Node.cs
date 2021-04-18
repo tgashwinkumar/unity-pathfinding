32fa6d1945710be311268d83698a45c21d4170d5
tgashwinkumar <tgashwinkumar@gmail.com>
2021-04-18 18:02:36 +0530

    Added files.

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node {
+    public bool walkable;
+    public Vector3 worldPosition;
+    public int gCost;
+    public int hCost;
+
+    public int gridX;
+    public int gridY;
+
+    public Node parentNode;
+    public Node(bool _walkable, Vector3 _worldPosition, int _gridX, int _gridY){
+        walkable = _walkable;
+        worldPosition = _worldPosition;
+        gridX = _gridX;
+        gridY = _gridY;
+    }
+    public int fCost{
+        get => gCost + hCost;
+    }
+}
